2dc6ae9bf98b72f6a7b83a5ff5c5f6b6ed0dc890
Jaroslaw Tanistra <j.tanistra@futuremind.com>
2024-11-08 10:59:37 +0100

    catch AttributeError exeception

--- pytest_retry_class/main.py
+++ pytest_retry_class/main.py
@@ -3,16 +3,16 @@ from _pytest.runner import runtestprotocol
 
 def pytest_addoption(parser):
     parser.addini(
-        name='maxretry',
+        name="maxretry",
         help="Number of times to retry a test class",
         type="string",
-        default=None
+        default=None,
     )
     parser.addini(
-        name='retry_on_exception',
+        name="retry_on_exception",
         help="Exceptions to retry on",
         type="linelist",
-        default=None
+        default=None,
     )
     parser.addoption("--maxretry", action="store", default=None)
     parser.addoption("--retry_on_exception", action="store", default=None)
@@ -20,11 +20,13 @@ def pytest_addoption(parser):
 
 def report_run(item):
     item.ihook.pytest_runtest_logstart(
-        nodeid=item.nodeid, location=item.location)
+        nodeid=item.nodeid, location=item.location
+    )
     for report in item.reports:
         item.ihook.pytest_runtest_logreport(report=report)
     item.ihook.pytest_runtest_logfinish(
-        nodeid=item.nodeid, location=item.location)
+        nodeid=item.nodeid, location=item.location
+    )
 
 
 def pytest_runtest_protocol(item, nextitem):
@@ -38,7 +40,7 @@ def pytest_runtest_protocol(item, nextitem):
     siblings = [item]
     items = item.session.items
     index = items.index(item)
-    for i in items[index+1:]:
+    for i in items[index + 1:]:
         if item.cls != i.cls:
             break
         siblings.append(i)
@@ -53,27 +55,38 @@ def pytest_runtest_protocol(item, nextitem):
     )
     cmd_retry_arg = config.getoption("retry_on_exception") or None
     handle_exceptions = (
-            cmd_retry_arg.strip("[]").split(", ") if cmd_retry_arg else []
-            or config.getini("retry_on_exception")
+        cmd_retry_arg.strip("[]").split(", ")
+        if cmd_retry_arg
+        else [] or config.getini("retry_on_exception")
     )
     exceptions = []
     while not all_passed and run_count <= max_run_count:
         all_passed = True
-        for i in range(len(siblings)-1):
+        for i in range(len(siblings) - 1):
             siblings[i].reports = runtestprotocol(
-                siblings[i], nextitem=siblings[i+1], log=False)
+                siblings[i], nextitem=siblings[i + 1], log=False
+            )
             all_passed = all_passed and all(
-                [rep.passed for rep in siblings[i].reports])
+                [rep.passed for rep in siblings[i].reports]
+            )
             failed_steps = [rep for rep in siblings[i].reports if rep.failed]
-            exceptions += [
-                step.longrepr.reprcrash.message.split(":")[0].split(".")[-1]
-                for step in failed_steps
-            ]
+            try:
+                exceptions += [
+                    step.longrepr.reprcrash.message.split(":")[0].split(".")[
+                        -1
+                    ]
+                    for step in failed_steps
+                ]
+            except AttributeError:
+                exceptions += [
+                    step.longrepr.errorstring.split(":")[0].split(".")[-1]
+                    for step in failed_steps
+                ]
             if not all_passed:
                 # Clean up setup state.
                 item.session._setupstate.stack = {}
         if handle_exceptions and not any(
-                [exc in handle_exceptions for exc in exceptions]
+            [exc in handle_exceptions for exc in exceptions]
         ):
             break
         run_count += 1
